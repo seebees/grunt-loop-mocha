d82e21fafb39b088e3f69b04c1fd2b4c305745cb
ryan <ryan@alexa.com>
2014-10-01 14:13:25 -0700

    Updates and tests

--- tasks/process-loop.js
+++ tasks/process-loop.js
@@ -1,10 +1,20 @@
-var child_process = require("child_process")
-var path = require('path')
-var util = require('util')
-var _ = require('lodash')
+var spawn = require("child_process").spawn
+var path  = require('path')
+var util  = require('util')
+var _     = require('lodash')
 var async = require('async')
 
-module.exports = function (grunt) {
+/**
+  * Main level.  Grunt is injected
+  * from the task.  _spawn is injected
+  * to help with tests.
+  */
+module.exports = function exports (grunt, _spawn) {
+  // injection here
+  if (_spawn) {
+    spawn = _spawn
+  }
+  // Give us a function that will work with async
   return function processLoop(op, cb) {
     // rip up the op into vars
     var filesSrc                      = op.filesSrc
@@ -17,9 +27,10 @@ module.exports = function (grunt) {
 
     var limit         = localMochaOptions.limit || 5
     var parallelType  = localMochaOptions.parallelType
+    var env           = _.merge(process.env, localOtherOptionsStringified)
 
     // pick a way to split up the work
-    if (parallelType = 'directory') {  // async by directory
+    if (parallelType === 'directory') {   // async by directory
       async
         .mapLimit(_.chain(filesSrc)
                     // group by the directory path
@@ -36,48 +47,57 @@ module.exports = function (grunt) {
                     // update the label, do the work
                     work(itLabel + ':' + args[1].replace('/', '-')
                         , args[0]
+                        , _.cloneDeep(env)
+                        , _.cloneDeep(localopts)
                         , _cb)
                 }
                 , cb)
-    } else if (parallelType = 'file') {// async by file
+    } else if (parallelType === 'file') { // async by file
       async
         .mapLimit(filesSrc
                 , limit
                 , function(file, _cb) {
                     // update the label, do the work
                     work(itLabel + ':' + file.replace('/', '-')
-                        , file
+                        , [file]
+                        , _.cloneDeep(env)
+                        , _.cloneDeep(localopts)
                         , _cb)
                 }
                 , cb)
-    }  else {                          // not async
-      work(itLabel, filesSrc, cb)
+    }  else {                             // not async
+      work(itLabel, filesSrc, env, localopts
+          , function(err, result) {
+              // to normalize results, we wrap this one in an
+              // array, even though there is only one.
+              cb(err, [result])
+            })
     }
 
-    function work(_itLabel, _filesSrc, _cb) {
+    function work(_itLabel, _filesSrc, _env, _op, _cb) {
 
       // inform the world that we are going to start
       grunt.log.writeln("[grunt-loop-mocha] iteration: ", _itLabel);
 
       // update the reporter file
       if (localMochaOptions.reporter === "xunit-file") {
-        process.env.XUNIT_FILE = reportLocation + "/xunit-" + _itLabel + ".xml";
-        grunt.log.writeln("[grunt-loop-mocha] xunit output: ", process.env.XUNIT_FILE);
+        // Only update the env for the spawned process.
+        _env.XUNIT_FILE = reportLocation + "/xunit-" + _itLabel + ".xml";
+        grunt.log.writeln("[grunt-loop-mocha] xunit output: ", _env.XUNIT_FILE);
       }
 
       // push the files into localopts
       _filesSrc.forEach(function (el) {
-        localopts.push(el);
+        _op.push(el);
       });
 
       // more notify
-      grunt.log.writeln("[grunt-loop-mocha] mocha argv: ", localopts.toString());
+      grunt.log.writeln("[grunt-loop-mocha] mocha argv: ", _op.toString());
 
       // start a process
-      var child = child_process
-                    .spawn(mocha_path
-                        , localopts
-                        , {env: _.merge(process.env, localOtherOptionsStringified)});
+      var child = spawn(mocha_path
+                      , _op
+                      , {env: _env});
 
       // pipe the output (in paralell this is going to be noisey)
       child.stdout.pipe(process.stdout)
